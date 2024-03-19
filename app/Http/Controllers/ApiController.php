932ab5a4a9cc96ec5b7200db46801d8250a40633
David wanjohi <davidwagura83@gmail.com>
2024-03-19 19:22:06 +0300

    create api controller

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function register(Request $request) 
+    {
+
+    }
+    
+
+    public function login(Request $request) 
+    {
+
+    }
+
+
+    public function profile() 
+    {
+
+    }
+
+
+    public function logout(Request $request) 
+    {
+
+    }
+
+}
