e6e308634fe0de7710af0799bf2de2dcfeb27f02
David wanjohi <davidwagura83@gmail.com>
2024-03-19 19:55:37 +0300

    register api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
     public function register(Request $request) 
     {
+        $request->validate ([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
 
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User registered successfully'
+        ]);
     }
-    
+
 
     public function login(Request $request) 
     {
